eb13c1d05ce63b79467aedc6ac3061f1fd0f5a1b
Terek <terek.arce@gmail.com>
2018-08-16 02:27:29 -0400

    GSE19804 Complete
    Tested and imported GSE19804 script.

--- importTools/R/GSE27562.R
+++ importTools/R/GSE27562.R
@@ -1,67 +1,44 @@
-# files should be put in FIT directory
-# if running from R console overwrite FIT.R with: commandArgs <- function() c("GSE27562","hgu133plus2")
+# Run SIT first with GSE27562 to ensure the series is imported
 
+notebook_dir <- getwd() # get the working directory
+main_dir <- dirname(dirname(notebook_dir)) # get two levels up
+gse_dir = file.path(main_dir,"GSE","GSE27562")
 
-process <- function(matrix, name)
-{
-  # Get only 3 classes
-  colsNames <- union( grep("normal", colnames(matrix), value = TRUE ), grep("malignant", colnames(matrix), value = TRUE ))
-  colsNames <- union( colsNames, grep("benign", colnames(matrix), value = TRUE ))
-  
-  # remove PBMC_ from matrix
-  result_matrix <- gsub("PBMC_", "", colnames(matrix) )
-  
-  col_numbers <- rev(c(1:10))
-  
-  # remove "_training_" from matrix
-  t_col_numbers <- paste(c("_training_"), col_numbers, sep = '', collapse = ' ')
-  t_col_numbers <- unlist(strsplit(t_col_numbers, split=" "))
-  
-  for (i in 1:length(t_col_numbers) )
-  {
-      result_matrix <- sub(t_col_numbers[i][1], "", result_matrix)
-  }
-  
-  col_numbers <- rev(c(1:47))
-  
-  # remove "_validation_" from matrix
-  t_col_numbers <- paste(c("_validation_"), col_numbers, sep = '', collapse = ' ')
-  t_col_numbers <- unlist(strsplit(t_col_numbers, split=" "))
-  
-  for (i in 1:length(t_col_numbers) )
-  {
-      result_matrix <- sub(t_col_numbers[i][1], "", result_matrix)
-  }
+setwd(gse_dir)
 
-  # Create classes
-  write.table(result_matrix, paste(name, "_GSE27562_ts_classes.txt",
-  sep=""), sep = "\t", row.names=FALSE, col.names=FALSE)
-  
-  # Get data from only 3 classes
-  colsNumbers <- union( grep("normal", colnames(matrix), value = FALSE ), grep("malignant", colnames(matrix), value = FALSE ))
-  colsNumbers <- union( colsNames, grep("benign", colnames(matrix), value = FALSE ))
-  
-  data <- matrix[colsNumbers]
+paste("Reading in filtered RMA data")
+matrix <- read.table("filteredRMA.txt",header=TRUE,row.names=1)
 
-  # Create data
-  write.table(data, paste(name, "_GSE27562_ts_data.txt", sep=""),
-  sep = "\t", row.names=FALSE, col.names=FALSE)
+paste("Cleaning up...")
+classes <- gsub("PBMC_", "", colnames(matrix))
+
+toRemove <- rev(c(1:10))
+toRemove <- paste(c("_training_"), toRemove, sep = '', collapse = ' ')
+toRemove <- unlist(strsplit(toRemove, split=" "))
+for (i in 1:length(toRemove)) {
+    classes <- sub(toRemove[i][1], "", classes)
 }
 
-setwd("~/Downloads/RFiles/GSE27562")
+toRemove <- rev(c(1:47))
+toRemove <- paste(c("_validation_"), toRemove, sep='', collapse=' ')
+toRemove <- unlist(strsplit(toRemove, split=" "))
+for (i in 1:length(toRemove))
+{
+    classes <- sub(toRemove[i][1], "", classes)
+}
 
-matrix.gcrma <- read.table("gcrma_mat.txt",header=TRUE,row.names=1)
-matrix.rma <- read.table("rma_mat.txt",header=TRUE,row.names=1)
-matrix.mas5 <- read.table("mas5_mat.txt",header=TRUE,row.names=1)
-matrix.orig <- read.table("orig_mat.txt",header=TRUE,row.names=1)
-matrix.gcrma_filt<- read.table("gcrma_mat_filt.txt",header=TRUE,row.names=1)
-matrix.rma_filt <- read.table("rma_mat_filt.txt",header=TRUE,row.names=1)
-matrix.mas5_filt <- read.table("mas5_mat_filt.txt",header=TRUE,row.names=1)
+colnames(matrix) <- classes
 
-process(matrix.mas5, "mas5")
-process(matrix.orig, "orig")
-process(matrix.rma, "rma")
-process(matrix.gcrma, "gcrma")
-process(matrix.rma_filt, "rma_filt")
-process(matrix.gcrma_filt, "gcrma_filt")
-process(matrix.mas5_filt, "mas5_filt")
+paste("Generating classes and data...")
+patterns <- c("normal", "benign", "malignant")
+expressions = matrix[ , grepl( paste(patterns, collapse="|") , names( matrix ) ) ]
+
+classes = gsub("\\..*","",colnames(expressions))
+classes = as.matrix(classes)
+classes = t(classes)
+write.table(classes,file.path(gse_dir,"classes.txt"),sep = "\t", quote = FALSE, row.names=FALSE, col.names=FALSE)
+
+expressions = t(expressions)
+write.table(expressions,file.path(gse_dir,"exprs.txt"),sep = "\t", row.names=FALSE, col.names=FALSE)
+
+paste("Expressions and classes generated successfully.")
